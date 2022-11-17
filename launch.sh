85b00bdb5ab647313fe0b928aa78dd0ad2855252
Iaio99 <glnvlln0@gmail.com>
2022-11-17 01:48:27 +0100

    Updated launch.sh

--- launch.sh
+++ launch.sh
@@ -7,11 +7,5 @@ export LOGIN_USER="LOGIN"
 export LOGIN_PASS="login"
 export ADMIN_USER="ADMIN"
 export ADMIN_PASS="admin"
-export PISCINA_USER="PISCINA"
-export PISCINA_PASS="piscina"
-export INSEGNANTE_USER="INSEGNANTE"
-export INSEGNANTE_PASS="insegnante"
-export ANONYMOUS_USER="ANONYMOUS"
-export ANONYMOUS_PASS="anonymous"
 
-./bin/pms
+./bin/
