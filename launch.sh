5a10b566d5d3b14d7f5e1a1d2948d5f0571fd786
Iaio99 <glnvlln0@gmail.com>
2022-11-21 16:39:35 +0100

    Updated launch script

--- launch.sh
+++ launch.sh
@@ -5,6 +5,8 @@ export PORT="3302"
 export DB="Pool_Management"
 export LOGIN_USER="LOGIN"
 export LOGIN_PASS="login"
+export CUSTOMER_USER=""
+export CUSTOMER_PASS=""
 export ADMIN_USER="ADMIN"
 export ADMIN_PASS="admin"
 
