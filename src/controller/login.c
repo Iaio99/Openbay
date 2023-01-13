225c1514be1ad8fe8e7a9c26d76c62def05cb0d3
Iaio99 <glnvlln0@gmail.com>
2023-01-13 13:36:07 +0100

    Removed pool and teacher user from client

--- src/controller/login.c
+++ src/controller/login.c
@@ -3,9 +3,7 @@
 
 #include "login.h"
 #include "user.h"
-#include "teacher.h"
-#include "addetto_comunale.h"
-#include "piscina.h"
+#include "admin.h"
 #include "../view/login.h"
 #include "../model/db.h"
 
@@ -18,14 +16,8 @@ bool login(void)
 	role_t role = attempt_login(&cred);
 
 	switch(role) {
-		case ADDETTO_COMUNALE:
+		case ADMIN:
 			administrator_controller();
-			break;
-		case PISCINA:
-			pool_controller(cred.username);
-			break;
-		case INSEGNANTE:
-			teacher_controller();
 			break;
 		case USER:
 			user_controller();
