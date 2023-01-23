636a4ee3672f0acec1d764344fa1eec9d6200b2b
Iaio99 <glnvlln0@gmail.com>
2023-01-23 14:39:42 +0100

    Refactoring

--- src/utils/validation.c
+++ src/utils/validation.c
@@ -6,11 +6,12 @@
 
 #include "validation.h"
 
-regex_t regex_date;
-regex_t regex_time;
+
 regex_t regex_cf;
 regex_t regex_credit_card_number;
 regex_t regex_cvv;
+regex_t regex_date;
+regex_t regex_time;
 
 
 static void print_regerror(int errcode, size_t length, regex_t *compiled)
@@ -29,17 +30,17 @@ bool init_validation(void)
 	int ret4;
 	int ret5;
 
-	ret1 = regcomp(&regex_date, "^([12][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))", REG_EXTENDED);
+	ret1 = regcomp(&regex_cf, "^(([A-Z]|[a-z]){6}[0-9]{2}([A-Z]|[a-z])[0-9]{2}([A-Z]|[a-z])[0-9]{3}([A-Z]|[a-z]))", REG_EXTENDED);
 	if(ret1) {
 		if(ret1 == REG_ESPACE) {
 			fprintf(stderr, "%s\n", strerror(ENOMEM));
 		} else {
-			fprintf(stderr, "Fatal error while setting up date validation regex.\n");
+			fprintf(stderr, "Fatal error while setting up time validation regex.\n");
 		}
 	}
 
-	ret2 = regcomp(&regex_time, "^([0-1]?[0-9]|2?[0-3]|[0-9]):([0-5][0-9]|[0-9])$", REG_EXTENDED);
-	if(ret2) {
+	ret2 = regcomp(&regex_credit_card_number, "^([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})", REG_EXTENDED);
+	if (ret2) {
 		if(ret2 == REG_ESPACE) {
 			fprintf(stderr, "%s\n", strerror(ENOMEM));
 		} else {
@@ -47,8 +48,8 @@ bool init_validation(void)
 		}
 	}
 
-	ret3 = regcomp(&regex_cf, "^(([A-Z]|[a-z]){6}[0-9]{2}([A-Z]|[a-z])[0-9]{2}([A-Z]|[a-z])[0-9]{3}([A-Z]|[a-z]))", REG_EXTENDED);
-	if(ret3) {
+	ret3 = regcomp(&regex_cvv, "^([0-9]{3})", REG_EXTENDED);
+	if (ret3) {
 		if(ret3 == REG_ESPACE) {
 			fprintf(stderr, "%s\n", strerror(ENOMEM));
 		} else {
@@ -56,17 +57,17 @@ bool init_validation(void)
 		}
 	}
 
-	ret4 = regcomp(&regex_credit_card_number, "^([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})", REG_EXTENDED);
-	if (ret4) {
+	ret4 = regcomp(&regex_date, "^([12][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))", REG_EXTENDED);
+	if(ret4) {
 		if(ret4 == REG_ESPACE) {
 			fprintf(stderr, "%s\n", strerror(ENOMEM));
 		} else {
-			fprintf(stderr, "Fatal error while setting up time validation regex.\n");
+			fprintf(stderr, "Fatal error while setting up date validation regex.\n");
 		}
 	}
 
-	ret5 = regcomp(&regex_cvv, "^([0-9]{3})", REG_EXTENDED);
-	if (ret5) {
+	ret5 = regcomp(&regex_time, "^([0-1]?[0-9]|2?[0-3]|[0-9]):([0-5][0-9]|[0-9])$", REG_EXTENDED);
+	if(ret5) {
 		if(ret5 == REG_ESPACE) {
 			fprintf(stderr, "%s\n", strerror(ENOMEM));
 		} else {
