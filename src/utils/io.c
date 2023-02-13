e29eca686bfc73e7f21d09cef899fc1b50c53360
Iaio99 <glnvlln0@gmail.com>
2023-02-13 17:59:32 +0100

    Cambiata la funzione di get input (lunghezza + 1)

--- src/utils/io.c
+++ src/utils/io.c
@@ -46,7 +46,7 @@ char *get_input(char *question, int len, char *buff, bool hide)
 	(void)hide;
 #endif
 
-	if(fgets(buff, len, stdin) != NULL) {
+	if(fgets(buff, len + 1, stdin) != NULL) {
 		buff[strcspn(buff, "\n")] = 0;
 	} else {
 		printf("EOF received, leaving...\n");
@@ -55,7 +55,7 @@ char *get_input(char *question, int len, char *buff, bool hide)
 	}
 
 	// Empty stdin
-	if(strlen(buff) + 1 == len) {
+	if(strlen(buff) + 1 == len + 1) {
 		int ch;
 		while(((ch = getchar()) != EOF) && (ch != '\n'));
 		if(ch == EOF) {
