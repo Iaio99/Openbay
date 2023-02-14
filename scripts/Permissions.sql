e99b8fda146244a1521ac845fcbc01df81069151
Iaio99 <glnvlln0@gmail.com>
2023-02-14 19:58:06 +0100

    Creazione script per i permessi di esecuzione sulle stored_procedures

---
+++ scripts/Permissions.sql
@@ -0,0 +1,9 @@
+GRANT EXECUTE ON PROCEDURE login TO role_login;
+GRANT EXECUTE ON PROCEDURE user_registration TO role_login;
+GRANT EXECUTE ON PROCEDURE fai_offerta TO role_user;
+GRANT EXECUTE ON PROCEDURE stato_aste_utente TO role_user;
+GRANT EXECUTE ON PROCEDURE imposta_controfferta TO role_user;
+GRANT EXECUTE ON PROCEDURE visualizza_aste_passate TO role_user;
+GRANT EXECUTE ON PROCEDURE visualizza_oggetti_asta TO role_user;
+GRANT EXECUTE ON PROCEDURE indici_asta TO role_admin;
+GRANT EXECUTE ON PROCEDURE inserisci_categoria TO role_admin;
