7c9b9df8a2fed44569112ed5dcd1bd168c1a94a9
Iaio99 <glnvlln0@gmail.com>
2023-02-17 14:25:48 +0100

    Rimosse le validazione di cf, cvv e numero della carta di credito

--- src/utils/validation.h
+++ src/utils/validation.h
@@ -6,6 +6,3 @@ extern bool init_validation(void);
 extern void fini_validation(void);
 extern bool validate_time(char *str);
 extern bool validate_date(char *str);
-extern bool validate_cf(char *str);
-extern bool validate_cvv(char *str);
-extern bool validate_credit_card_number(char *str);
